6cf850f09e6be32176e03d6dfe4a55db4b9bc634
sebastian.radu <g.cont.sebi@gmail.com>
2022-02-25 10:27:14 +0200

    S6-production-vs-development-builds
    - add production and development config for webpack
    - add scripts to build in production and in development
    - add webpack-dev-server to serve file after build

--- src/index.js
+++ src/index.js
@@ -9,3 +9,9 @@ heading.render();
 helloWorldButton.render();
 
 // addImage();
+
+if (process.env.NODE_ENV === 'production') {
+    console.log('Production mode');
+} else if (process.env.NODE_ENV === 'development') {
+    console.log('Development mode')
+}
